2be380467b00fe87a83a43cf25762c3bbf9734e0
Dewi Jones <dewijones92@gmail.com>
2021-09-25 16:18:22 +0100

    set script flags

--- buildAndInstall.sh
+++ buildAndInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x;
+set -e;
 ./gradlew  build
 apksigner sign --ks-pass pass:123123 --ks my-release-key.keystore ./app/build/outputs/apk/latest/release/app-latest-release-unsigned.apk
 adb install  ./app/build/outputs/apk/latest/release/app-latest-release-unsigned.apk
